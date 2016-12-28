446fcd52607747fe47184cf525604a2d1235318d
rmanadhar <rmanadhar@RakeshPC>
2016-12-27 19:18:59 -0600

    now pushing new code to git hub

--- test/src/testing/Automationtest.java
+++ test/src/testing/Automationtest.java
@@ -5,6 +5,7 @@ public class Automationtest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my first git hub test");
+		System.out.println("this is my change to git hub test");
 	}
 
 }
