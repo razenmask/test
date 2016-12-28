acbd48e0173f18ce56c24b86cc7dbf0b0dad43b7
rmanadhar <rmanadhar@RakeshPC>
2016-12-27 19:13:19 -0600

    created local repo, and pushed the java project to git hub

---
+++ test/src/testing/Automationtest.java
@@ -0,0 +1,10 @@
+package testing;
+
+public class Automationtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first git hub test");
+	}
+
+}
